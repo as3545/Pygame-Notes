da6e3675151e4ecc988a174263749c79cfb8f465
as3545 <86479682+as3545@users.noreply.github.com>
2023-09-26 22:58:13 +0530

    notes.py

--- notes.py
+++ notes.py
@@ -169,3 +169,13 @@ hitSound.play()
 # Notice that we have not written -1 inside the parenthesis
 
         
+Function	Description
+init()	Initializes all of the imported Pygame modules (returns a tuple indicating success and failure of initializations)
+display.set_mode()	Takes a tuple or a list as its parameter to create a surface (tuple preferred)
+update()	Updates the screen
+quit()	Used to uninitialized everything
+set_caption()	Will set the caption text on the top of the display screen
+event.get()	Returns list of all events
+Surface.fill()	Will fill the surface with a solid color
+time.Clock()	Helps track time
+font.SysFont()	Will create a Pygame font from the System font resources
